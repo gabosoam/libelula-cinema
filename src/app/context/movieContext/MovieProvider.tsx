18436c2bd73a3684eaf390a9baa27f9347723c26
Gabriel Salazar Ortiz <gsalazar@libelulasoft.com>
2024-06-05 14:13:12 -0500

    Corrección de error en deploy

--- src/app/context/movieContext/MovieProvider.tsx
+++ src/app/context/movieContext/MovieProvider.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState, useEffect } from 'react';
 import { MovieContext } from "./MovieContext";
 import { getPopularMoviesFetch, searchMoviesFetch } from '@/lib/tmdb';
